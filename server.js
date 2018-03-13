93323032274cf60e63152355f17c7ad0ac53b533
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-12 19:33:41 -0500

    ready to add collaborators to project

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
