1db8f3a7aa1513d842a14e7b3a30b30cc5e33111
Mike D <michael.mcdaniel210@gmail.com>
2018-03-14 21:00:14 -0500

    changed db names to match variables in controllers

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,37 +4,37 @@ module.exports = function (app) {
 
     //GET route for all events:
     app.get("/api/events/", function (req, res) {
-        db.Event.findAll({}).then(function (dbEvent) {
-            res.json(dbEvent);
+        db.events.findAll({}).then(function (dbevents) {
+            res.json(dbevents);
         });
     });
     //POST route for saving an event:
     app.post("/api/events", function (req, res) {
         // console.log(req.body);
-        db.Event.create({
+        db.events.create({
             // title: req.body.title,
             // body: req.body.body,
             // category: req.body.category
         })
-            .then(function (dbEvent) {
-                res.json(dbEvent);
+            .then(function (dbevents) {
+                res.json(dbevents);
             });
     });
     //GET route for events and attendees and items:
     app.get("/api/guests", function (req, res) {
-        db.Guest.findAll({}).then(function (dbGuest) {
-            res.json(dbGuest);
+        db.guests.findAll({}).then(function (dbguests) {
+            res.json(dbguests);
         });
 
     })
     //DELETE route guests
     app.delete("/api/guests/:id", function (req, res) {
-        db.Guest.destroy({
+        db.guests.destroy({
             where: {
                 id: req.params.id
             }
-        }).then(function (dbGuest) {
-            res.json(dbGuest);
+        }).then(function (dbguests) {
+            res.json(dbguests);
         });
     });
 };
