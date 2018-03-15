34574af00c7620e44600bca272e653c306b512f0
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-15 16:01:05 -0500

    adding more routes

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 var express                 = require("express"),
+    app                     = express(),
     passport                = require("passport"),
-    env                     = require("dotenv"),
-    models                  = require("./models"),
-    bodyParser              = require("body-parser"),
     session                 = require('express-session'),
+    bodyParser              = require("body-parser"),
+    env                     = require("dotenv"),
     User                    = require("./models/users"),
     localStrategy           = require("passport-local"),
     path                    = require("path"),
     exphbs                  = require("express-handlebars");
 
-var app = express();
-
-
 var PORT = process.env.PORT || 3000;
-
-app.set("view engine", "handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
  
+app.get("/", function(req, res) {
+    res.send("index");
+});
+
+var models = require("./models");
+var authRoute = require("./routes/auth.js")(app);
+
+require('./config/passport/passport.js')(passport, models.users);
+
 models.sequelize.sync().then(function() {
     console.log('Nice! Database looks fine')
  
@@ -35,10 +39,6 @@ models.sequelize.sync().then(function() {
  
 });
 
-
-app.get("/", function(req, res) {
-    res.send("home");
-});
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
