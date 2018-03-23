a8419584a30dcf4df5e8e03deef3bab5f3482da2
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-23 18:31:40 -0500

    only showing events related to that specific user

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,82 +2,84 @@ var db = require("../models");
 var exports = module.exports = {}
 
 exports.signup = function (req, res) {
-    res.render('signup');
+	res.render('signup');
 }
 
 exports.signin = function (req, res) {
-    console.log(req.flash('error'));
-    res.render('signin', { message: req.flash('error') });
+	console.log(req.flash('error'));
+	res.render('signin', { message: req.flash('error') });
 }
 
 exports.create = function (req, res) {
-    var first = req.user.firstname;
-    var last = req.user.lastname;
-    var user_id = req.user.id;
-    res.render('create', { first: first, last: last, user_id: user_id });
+	var first = req.user.firstname;
+	var last = req.user.lastname;
+	var user_id = req.user.id;
+	res.render('create', { first: first, last: last, user_id: user_id });
 }
 
 exports.oneEvent = function (req, res) {
-    var first = req.user.firstname;
-    var last = req.user.lastname;
-    var idEvent = req.params.id;
-    db.events.findOne({
-        where: {
-            id: idEvent
-        }
-    }).then(function (data) {
-        var event = data;
-        console.log(event);
-        res.render("events", { first: first, last: last, event: event });
-        
-    });
+	var first = req.user.firstname;
+	var last = req.user.lastname;
+	var idEvent = req.params.id;
+	db.events.findOne({
+			where: {
+					id: idEvent
+			}
+	}).then(function (data) {
+		var event = data;
+		console.log(event);
+		res.render("events", { first: first, last: last, event: event });		
+	});
 }   
 
 exports.home = function(req, res) {
-        var first = req.user.firstname;
-        var last = req.user.lastname;
-        var user_id = req.user.id;
-        db.events.findAll({})
-            .then(function (dbevents) {
-                var events = dbevents;
-             res.render("home", {first: first, last: last, user_id: user_id, events: events});
-        });
-    }
+	var first = req.user.firstname;
+	var last = req.user.lastname;
+	var user_id = req.user.id;
+	db.events.findAll({
+	where: {
+			userId: user_id
+	}     
+	})
+	.then(function (dbevents) {
+		var events = dbevents;
+		res.render("home", {first: first, last: last, user_id: user_id, events: events});
+	});
+}
 
 exports.updateOneEvent = function (req, res) {
-            var first = req.user.firstname;
-            var last = req.user.lastname;
-            var updEvent = req.params.id;
-            db.events.findOne({
-                where: {
-                    id: updEvent
-                }
-            }).then(function (data) {
-                var event = data;
-                console.log(event);
-                res.render("update_event", { first: first, last: last, event: event });
-            });
-        }
+	var first = req.user.firstname;
+	var last = req.user.lastname;
+	var updEvent = req.params.id;
+	db.events.findOne({
+		where: {
+				id: updEvent
+		}
+	}).then(function (data) {
+		var event = data;
+		console.log(event);
+		res.render("update_event", { first: first, last: last, event: event });
+	});
+}
 
 exports.update = function (req, res) {
-            var first = req.user.firstname;
-            var last = req.user.lastname;
-            var user_id = req.user.id;
-            var toUpdate = req.params.id;
-            db.events.update({
-                where: {
-                    id: toUpdate
-                }
-            }).then(function (data) {
-                ß
-                var event = data;
-                console.log(event);
-                res.render("events", { first: first, last: last, user_id: user_id, event: event });
-            });
-        }
+	var first = req.user.firstname;
+	var last = req.user.lastname;
+	var user_id = req.user.id;
+	var toUpdate = req.params.id;
+	db.events.update({
+			where: {
+					id: toUpdate
+			}
+	}).then(function (data) {
+		var event = data;
+		console.log(event);
+		res.render("events", { first: first, last: last, user_id: user_id, event: event });
+	});
+}
 
 exports.logout = function (req, res) {
-            req.session.destroy(function (err) {
-                res.redirect('/');
-            });
-        }
+	req.session.destroy(function (err) {
+			res.redirect('/');
+	});
+}
