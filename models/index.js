bfec54e1003cf2928a74b7426fa3f10984af676c
Mike D <michael.mcdaniel210@gmail.com>
2018-03-23 20:37:41 -0500

    added jawsdb code

--- models/index.js
+++ models/index.js
@@ -7,7 +7,10 @@ var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
-
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else
 var express= require("express");
 var app= express();
 var bodyParser= require("body-parser");
